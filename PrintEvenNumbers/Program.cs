3f7b8d13f30454fd40be7219995d3156f3112a4a
YoginiMac12 <113415806+YoginiMac12@users.noreply.github.com>
2022-09-22 22:24:12 +0530

    Add files via upload

---
+++ PrintEvenNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {  
+            int EvenCount = 0;
+            Console.WriteLine("1 to 50 Even Number:");
+            for (int i=1;i<=50;i++)
+            {
+                if(i%2==0)
+                {
+                   Console.WriteLine(i);
+                   EvenCount++;
+                }
+            }
+            Console.WriteLine("Total Even Number Count=" +  EvenCount);
+          { 
+           Console.ReadLine();
+          }
+        }
+    }
+}
+
+
+    
+
+    
+    
+
